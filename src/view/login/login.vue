15c9da642b21a3ae5bef161c5a7b323e89bf05e1
zhaoxiang <zhaoxiang051405@gmail.com>
2019-06-21 09:38:55 +0800

    modified 整合登录组件

--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -7,7 +7,25 @@
     <div class="login-con">
       <Card icon="log-in" title="欢迎登录" :bordered="false">
         <div class="form-con">
-          <login-form @on-success-valid="handleSubmit"></login-form>
+          <Form ref="loginForm" :model="form" :rules="rules">
+            <FormItem prop="username">
+              <Input v-model="form.username" placeholder="请使用微信/QQ登录">
+                <span slot="prepend">
+                  <Icon :size="16" type="ios-person"></Icon>
+                </span>
+              </Input>
+            </FormItem>
+            <FormItem prop="password">
+              <Input type="password" v-model="form.password" placeholder="请使用微信/QQ登录">
+                <span slot="prepend">
+                  <Icon :size="14" type="md-lock"></Icon>
+                </span>
+              </Input>
+            </FormItem>
+            <FormItem style="margin-bottom: 15px;">
+              <Button @click="handleSubmit" type="primary" long>登录</Button>
+            </FormItem>
+          </Form>
           <div style="padding-top:10px;font-size: 11px;border-top: 1px solid #e9eaec;">
             <span style="vertical-align:middle;">其他登录：</span>
             <img style="vertical-align:middle;" :src="qq_login" alt=""> &nbsp;
@@ -21,30 +39,44 @@
 </template>
 
 <script>
-import LoginForm from '_c/login-form'
 import { mapActions } from 'vuex'
 import qq_login from '@/assets/images/qq_login.png'
 import wx_login from '@/assets/images/wx_login.png'
 export default {
-  components: {
-    LoginForm
-  },
   data () {
     return {
       qq_login,
-      wx_login
+      wx_login,
+      form: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          {required: true, message: '账号不能为空', trigger: 'blur'}
+        ],
+        password: [
+          {required: true, message: '密码不能为空', trigger: 'blur'}
+        ]
+      }
     }
   },
   methods: {
     ...mapActions([
-      'handleLogin',
-      'getUserInfo'
+      'handleLogin'
     ]),
-    handleSubmit ({ username, password }) {
-      this.handleLogin({ username, password }).then(res => {
-        this.$router.push({
-          name: 'home'
-        })
+    handleSubmit () {
+      let vm = this;
+      let username = vm.form.username;
+      let password = vm.form.password;
+      vm.$refs.loginForm.validate((valid) => {
+        if (valid) {
+          vm.handleLogin({ username, password }).then(res => {
+            vm.$router.push({
+              name: 'home'
+            })
+          })
+        }
       })
     }
   }
